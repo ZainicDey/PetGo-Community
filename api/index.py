1c9057bbaa624eae3ac49834c5ba41eca1fbc1a6
Anik Dey <ihanik.ad@gmail.com>
2026-09-26 03:40:39 +0600

    fix: export app at top-level for vercel

--- api/index.py
+++ api/index.py
@@ -6,23 +6,4 @@ root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
-try:
-    from app.main import app
-except Exception as e:
-    import traceback
-    error_trace = traceback.format_exc()
-    error_message_bytes = f"FastAPI Startup Exception on Vercel:\n\n{error_trace}".encode("utf-8")
-
-    async def app(scope, receive, send):
-        assert scope['type'] == 'http'
-        await send({
-            'type': 'http.response.start',
-            'status': 500,
-            'headers': [
-                (b'content-type', b'text/plain'),
-            ]
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': error_message_bytes,
-        })
+from app.main import app
